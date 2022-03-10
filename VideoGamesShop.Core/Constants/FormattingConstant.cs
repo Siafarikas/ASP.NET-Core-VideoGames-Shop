f8923c020b27848acb686adb2d65841b88de0e85
Alexandros Siafarikas <siafarikasalexandros@gmail.com>
2022-03-10 02:19:44 +0200

    Model binders added

---
+++ VideoGamesShop.Core/Constants/FormattingConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace VideoGamesShop.Core.Constants
+{
+    public class FormattingConstant
+    {
+        public const string NormalDateFormat = "dd.MM.yyyy";
+    }
+}
--- VideoGamesShop/Program.cs
+++ VideoGamesShop/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using VideoGamesShop.Core.Constants;
 using VideoGamesShop.Data;
+using VideoGamesShop.ModelBinders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,13 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddMvcOptions(options => 
+    {
+        options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+        options.ModelBinderProviders.Insert(1, new DateTimeModelBinderProvider(FormattingConstant.NormalDateFormat));
+        options.ModelBinderProviders.Insert(2, new DoubleModelBinderProvider());
+    });
 
 var app = builder.Build();
 
