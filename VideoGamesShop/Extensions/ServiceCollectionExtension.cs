4dfd43f4340101cb5f0e54b9689964e3eb780200
Alexandros Siafarikas <siafarikasalexandros@gmail.com>
2022-03-21 13:44:46 +0200

    Service collection extension methods

---
+++ VideoGamesShop/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGamesShop.Infrastructure.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
