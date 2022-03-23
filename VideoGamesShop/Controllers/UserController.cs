1fa71cab525f216e3a5c69b9c709ff4a1d657669
Alexandros Siafarikas <siafarikasalexandros@gmail.com>
2022-03-23 16:49:33 +0200

    User controller added

--- VideoGamesShop/Controllers/UserController.cs
+++ VideoGamesShop/Controllers/UserController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using VideoGamesShop.Core.Constants;
+using VideoGamesShop.Core.Contracts;
+using VideoGamesShop.Infrastructure.Data.Identity;
 
 namespace VideoGamesShop.Controllers
 {
@@ -7,13 +11,32 @@ namespace VideoGamesShop.Controllers
     {
         private readonly RoleManager<IdentityRole> roleManager;
 
-        public UserController(RoleManager<IdentityRole> _roleManager)
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private readonly IUserService userService;
+
+        public UserController(
+            RoleManager<IdentityRole> _roleManager,
+            UserManager<ApplicationUser> _userManager,
+            IUserService _userService
+            )
         {
             roleManager = _roleManager;
+            userManager = _userManager;
+            userService = _userService;
         }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Authorize(Roles = UserConstants.Roles.Administrator)]
+        public async Task<IActionResult> ManageUsers()
+        {
+            var users = await userService.GetUsers();
+
+
+            return Ok(users);
         }
 
         public async Task<ActionResult> CreateRole()
