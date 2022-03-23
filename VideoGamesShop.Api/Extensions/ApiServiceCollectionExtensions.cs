4c9050e2da6d0598c281b687bd2ff402e4031571
Alexandros Siafarikas <siafarikasalexandros@gmail.com>
2022-03-23 16:50:08 +0200

    Api added

---
+++ VideoGamesShop.Api/Extensions/ApiServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGamesShop.Infrastructure.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ApiServiceCollectionExtension
+    {
+        public static IServiceCollection AddApiDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
