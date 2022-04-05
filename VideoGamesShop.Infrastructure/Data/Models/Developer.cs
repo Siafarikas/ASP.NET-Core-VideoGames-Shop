e0666070d9b6a0576f6ae3c06035872281b60550
Alexandros Siafarikas <siafarikasalexandros@gmail.com>
2022-04-06 02:55:54 +0300

    Developer first and last name added

--- VideoGamesShop.Infrastructure/Data/Models/Developer.cs
+++ VideoGamesShop.Infrastructure/Data/Models/Developer.cs
@@ -6,6 +6,11 @@ namespace VideoGamesShop.Infrastructure.Data.Models
     {
         [Key]
         public string Id { get; set; } 
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
         public IList<Game> Games { get; set; } = new List<Game>();
     }
 }
