ea90c9f5f6475ac57b571cf309e2141c7bf7a4af
Alexandros Siafarikas <siafarikasalexandros@gmail.com>
2022-04-09 01:09:26 +0300

    Statistics page for developer added

---
+++ VideoGamesShop.Core/Models/Developer/StatisticsPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoGamesShop.Infrastructure.Data.Models;
+
+namespace VideoGamesShop.Core.Models.Developer
+{
+    public class StatisticsPageViewModel
+    {
+        public int SalesCount { get; set; }
+
+        public decimal? Revenue { get; set; }
+
+        //public int Customers { get; set; }
+
+        //public ICollection<Purchase> Sales { get; set; } 
+    }
+}
