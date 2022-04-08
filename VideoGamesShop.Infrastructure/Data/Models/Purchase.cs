043d332ad753d6c5533bf354d7fb58d8eb94722f
Alexandros Siafarikas <siafarikasalexandros@gmail.com>
2022-04-08 20:33:54 +0300

    Purchase model for users library

---
+++ VideoGamesShop.Infrastructure/Data/Models/Purchase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoGamesShop.Infrastructure.Data.Identity;
+
+namespace VideoGamesShop.Infrastructure.Data.Models
+{
+    public class Purchase
+    {
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public string GameId { get; set; }
+        public Game Game { get; set; }
+    }
+}
