e4bf9657dc3c5199507b370359e0c7b92869ee2a
Alexandros Siafarikas <siafarikasalexandros@gmail.com>
2022-04-11 18:15:11 +0300

    Pagination implemented via pager partial view

--- VideoGamesShop/Controllers/StoreController.cs
+++ VideoGamesShop/Controllers/StoreController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VideoGamesShop.Core.Contracts;
+using VideoGamesShop.Core.Models.Game;
 
 namespace VideoGamesShop.Controllers
 {
@@ -18,16 +19,32 @@ namespace VideoGamesShop.Controllers
         }
 
         [Authorize]
-        public async Task<IActionResult> Games()
+        public async Task<IActionResult> Games(int pg = 1)
         {
             var games = await gameService.GetGames();
 
-            return View(games);
+            const int pageSize = 2;
+            if (pg < 1)
+            {
+                pg = 1;
+            }
+
+            int recsCount = games.Count();
+
+            var pager = new Pager(recsCount, pg, pageSize);
+
+            int recSkip = (pg - 1) * pageSize;
+
+            var data = games.Skip(recSkip).Take(pager.PageSize).ToList();
+
+            this.ViewBag.Pager = pager;
+
+            return View(data);
         }
 
         [Authorize]
         public async Task<IActionResult> AddToCart(string userId, string gameId)
-         {
+        {
             var addedToCart = await cartService.AddToCart(userId, gameId);
 
             if (addedToCart == true)
@@ -38,7 +55,7 @@ namespace VideoGamesShop.Controllers
             {
                 //TempData[GlobalMessageKey] = "An error accured";
             }
-            return RedirectToAction("MyCart", "Cart", new {userId = userId});
+            return RedirectToAction("MyCart", "Cart", new { userId = userId });
         }
 
         [Authorize]
